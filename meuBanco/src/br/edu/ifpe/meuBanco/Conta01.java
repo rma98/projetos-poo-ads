9e65b650c04f43eb99d1c69f955c142d1c96df76
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-04 19:55:55 -0300

    Criando uma classe Conta01 do tipo Generics

---
+++ meuBanco/src/br/edu/ifpe/meuBanco/Conta01.java
@@ -0,0 +1,38 @@
+package br.edu.ifpe.meuBanco;
+
+public class Conta01<T> {
+	private T numConta;
+	private T numAgencia;
+	private T saldo;
+	
+	public Conta01(T numConta, T numAgencia, T saldo) {
+		this.numConta = numConta;
+		this.numAgencia = numAgencia;
+		this.saldo = saldo;
+	}
+
+	public T getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(T numConta) {
+		this.numConta = numConta;
+	}
+
+	public T getNumAgencia() {
+		return numAgencia;
+	}
+
+	public void setNumAgencia(T numAgencia) {
+		this.numAgencia = numAgencia;
+	}
+
+	public T getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(T saldo) {
+		this.saldo = saldo;
+	}
+	
+}
