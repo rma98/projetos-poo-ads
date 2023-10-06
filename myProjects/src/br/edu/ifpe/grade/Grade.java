17bd9289f92fe52ef7daa0c7369841dfb14735ad
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-06 18:03:19 -0300

    Update

--- myProjects/src/br/edu/ifpe/grade/Grade.java
+++ myProjects/src/br/edu/ifpe/grade/Grade.java
@@ -1,10 +1,12 @@
 package br.edu.ifpe.grade;
 
 public class Grade<T extends Comparable<T>> {
+	private String nome;
 	private T nota01;
 	private T nota02;
 	
-	public Grade(T nota01, T nota02) {
+	public Grade(String nome,T nota01, T nota02) {
+		this.nome = nome;
 		this.nota01 = nota01;
 		this.nota02 = nota02;
 	}
