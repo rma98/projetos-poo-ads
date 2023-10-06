c669dff3600222f32202571b0927e4f23e1de38b
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-06 17:06:27 -0300

    Update

--- meuBanco/src/br/edu/ifpe/banco/Main.java
+++ meuBanco/src/br/edu/ifpe/banco/Main.java
@@ -6,13 +6,21 @@ public class Main {
 		ContaCorrente contaA = new ContaCorrente("1010", "01", 500.0);
 		ContaCorrente contaB = new ContaCorrente("2020", "02", 200.0);
 		
+		OperacoesContaCorrente operacoesContaCorrente = new OperacoesContaCorrente(); 
+		
 		Double saldoAtual;
 		
 		saldoAtual = contaA.getSaldo();
 		System.out.println("Saldo atual da conta A: " + saldoAtual);
+		operacoesContaCorrente.creditarConta(contaA, 1200.0);
+		saldoAtual = contaA.getSaldo();
+		System.out.println("Saldo após crédito: " + saldoAtual);
 		
 		saldoAtual = contaB.getSaldo();
 		System.out.println("Saldo atual da conta B: " + saldoAtual);
+		operacoesContaCorrente.creditarConta(contaB, 300.0);
+		saldoAtual = contaB.getSaldo();
+		System.out.println("Saldo após crédito: " + saldoAtual);
 
 	}
 
