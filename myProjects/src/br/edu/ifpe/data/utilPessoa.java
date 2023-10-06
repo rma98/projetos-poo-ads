42cdea40b5fcfde21ba238351f3963913b2f2f5e
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-06 20:56:54 -0300

    Criando a classe UtilPessoa

---
+++ myProjects/src/br/edu/ifpe/data/utilPessoa.java
@@ -0,0 +1,31 @@
+package br.edu.ifpe.data;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class utilPessoa {
+	public static void imprimirDados(Pessoa pessoa) {
+		System.out.println("Nome: " + pessoa.getNome());
+		System.out.println("Ano de Nascimento: " + pessoa.getAnoNascimento());
+		System.out.println("Altura: " + pessoa.getAltura());
+	}
+	
+	public static int calcularIdade(Pessoa pessoa) {
+		// Obtém o ano de nascimento da pessoa como String
+		String anoNascimentoStr = pessoa.getAnoNascimento();
+		
+		// Converte a String para um objeto LocalDate
+		LocalDate dataNascimento = LocalDate.parse(anoNascimentoStr);
+		
+		// Obtém a data atual
+		LocalDate dataAtual = LocalDate.now();
+		
+		// Calcula a diferença entre as datas
+		Period periodo = Period.between(dataNascimento, dataAtual);
+		
+		// Obtém a idade a partir do período
+		int idade = periodo.getYears();
+		
+		return idade;
+	}
+}
