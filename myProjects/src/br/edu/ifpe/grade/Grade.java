d166d8b52f8efa3bdaf4454ea0c42ecd5f00c0b8
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-06 17:45:03 -0300

    Criando a classe Grade

---
+++ myProjects/src/br/edu/ifpe/grade/Grade.java
@@ -0,0 +1,28 @@
+package br.edu.ifpe.grade;
+
+public class Grade<T extends Comparable<T>> {
+	private T nota01;
+	private T nota02;
+	
+	public Grade(T nota01, T nota02) {
+		this.nota01 = nota01;
+		this.nota02 = nota02;
+	}
+
+	public T getNota01() {
+		return nota01;
+	}
+
+	public void setNota01(T nota01) {
+		this.nota01 = nota01;
+	}
+
+	public T getNota02() {
+		return nota02;
+	}
+
+	public void setNota02(T nota02) {
+		this.nota02 = nota02;
+	}
+	
+}
