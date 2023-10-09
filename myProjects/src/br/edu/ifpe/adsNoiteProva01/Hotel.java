dc7cc934f83c02442bacbeb7d127630de6ba71b7
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-09 15:30:57 -0300

    Criando a classe Hotel

---
+++ myProjects/src/br/edu/ifpe/adsNoiteProva01/Hotel.java
@@ -0,0 +1,22 @@
+package br.edu.ifpe.adsNoiteProva01;
+
+public class Hotel {
+	private String nome;
+	
+	public Hotel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Hotel(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
