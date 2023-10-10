26610db4d6393bedcac2368106042cb9008c5cad
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-10 17:02:24 -0300

    Criando a classe Aluno

---
+++ myProjects/src/br/edu/ifpe/aluno/Aluno.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.aluno;
+
+public class Aluno {
+	protected String nome;
+	protected int matricula;
+	protected double mediaGlobal;
+	
+	public Aluno(String nome, int matricula, double mediaGlobal) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.mediaGlobal = mediaGlobal;
+	}
+}
