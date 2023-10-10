9ab54150ec776d2050ffba2a8a6669aa509e7fb3
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-10 10:16:54 -0300

    Update

--- myProjects/src/br/edu/ifpe/adsManhaProva01/AppPrincipal.java
+++ myProjects/src/br/edu/ifpe/adsManhaProva01/AppPrincipal.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class AppPrincipal {
 	
+	static List<Veiculo> veiculos = new ArrayList<>();
 	static List<OrdemServico> ordemServicos = new ArrayList<>();
 	static List<Servico> servicos = new ArrayList<>();
 
@@ -15,6 +16,8 @@ public class AppPrincipal {
 		Pessoa cliente = new Cliente("José", "0369147", "Tipo 01");
 		
 		Veiculo veiculo = new Veiculo("Placa 01", "Marca 01", "Modelo 01", (Cliente) cliente);
+		
+		veiculos.add(veiculo);
 		
 		Servico servico = new Servico("Óleo de Motor", 280.0);
 		Servico servico2 = new Servico("Filtro de ar", 320.0);
@@ -26,31 +29,38 @@ public class AppPrincipal {
 		ordemServicos.add(ordemServico);
 		ordemServicos.add(ordemServico2);
 		
-		ordemServico.getServicos().add(servico);
-		ordemServico2.getServicos().add(servico2);
+		veiculo.getOrdemServicos().add(ordemServico);
+		veiculo.getOrdemServicos().add(ordemServico2);
 		
 		servicos.add(servico);
 		servicos.add(servico2);
 		servicos.add(servico3);
 		
-		for (Servico ser : servicos) {
-			System.out.println("Placa: " + veiculo.getNumPlaca());
-			System.out.println("Marca: " + veiculo.getMarca());
-			System.out.println("Modelo: " + veiculo.getModelo());
-			System.out.println("Nome do Cliente: " + veiculo.getCliente().getNome());
-			System.out.println("Rg: " + veiculo.getCliente().getRg());
-			System.out.println("Tipo do Cliente: " + veiculo.getCliente().getTipoCliente());
-			System.out.println("----------------------------------------");
-			System.out.println("Descrição: " + ser.getDescricao());
-			System.out.println("Valor: " + ser.getValor());
-			for (OrdemServico ordSer : ordemServicos) {
-				System.out.println("----------------------------------------");
-				System.out.println("NumOS: " + ordSer.getNumOS());
-				System.out.println("Data Exo: " + ordSer.getDataExo());
-				System.out.println("Data de Conclusão: " + ordSer.getDataConclusao());
-				System.out.println("Nome do Funcionário: " + ordSer.getFuncionario().getNome());
+		ordemServico.getServicos().add(servico);
+		ordemServico2.getServicos().add(servico2);
+		
+		for (Veiculo v : veiculos) {
+			System.out.println("----------------- VEÍCULO -----------------------------------");
+			System.out.println("Número da Placa: " + v.getNumPlaca());
+			System.out.println("Modelo: " + v.getModelo());
+			System.out.println("Marca: " + v.getMarca());
+			System.out.println("------------------ CLIENTE ----------------------------------");
+			System.out.println("Cliente: " + v.getCliente().getNome());
+			System.out.println("RG: " + v.getCliente().getRg());
+			System.out.println("Tipo do Cliente: " + v.getCliente().getTipoCliente());
+			for (OrdemServico os : v.getOrdemServicos()) {
+				System.out.println("--------------- ORDEM DE SERVIÇO -------------------------------------");
+				System.out.println("Data OS: " + os.getNumOS());
+				System.out.println("Data Exo: " + os.getDataExo());
+				System.out.println("Data de Conclusão: " + os.getDataConclusao());
+				System.out.println("Nome do funcionário que realizou o atendimento: " + os.getFuncionario().getNome());
+				for (Servico s : os.getServicos()) {
+					System.out.println("---------------------- SERVIÇO ------------------------------");
+					System.out.println("Descrição do serviço: " + s.getDescricao());
+					System.out.println("Valor: " + s.getValor());
+				}
 			}
-			System.out.println("----------------------------------------");
+			System.out.println("--------------------------------------------------------------");
 		}
 
 	}
