4feeba48bfce984f5cd9b3eeaeb9858f81944f91
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-10 16:38:16 -0300

    Criando a classe Student

---
+++ myProjects/src/br/edu/ifpe/student/Student.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.student;
+
+public class Student {
+	String nome;
+	int matricula;
+	double mediaGlobal;
+	
+	public Student(String nome, int matricula, double mediaGlobal) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.mediaGlobal = mediaGlobal;
+	}
+}
