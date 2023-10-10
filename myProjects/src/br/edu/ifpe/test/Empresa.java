fa71e63c8375ba1333deb8ae9162f57666c086eb
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-10 01:27:37 -0300

    Criando a classe Empresa

---
+++ myProjects/src/br/edu/ifpe/test/Empresa.java
@@ -0,0 +1,18 @@
+package br.edu.ifpe.test;
+
+public class Empresa {
+	private String nome;
+	
+	public Empresa(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
