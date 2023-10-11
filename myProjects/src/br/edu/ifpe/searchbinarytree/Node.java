383b1cd322fef2d62fff42f26a48e9043076b624
Robson Albuquerque <rma@a.palmares.ifpe.edu.br>
2023-10-11 14:14:40 -0300

    Criando a classe Node

---
+++ myProjects/src/br/edu/ifpe/searchbinarytree/Node.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.searchbinarytree;
+
+public class Node<T> {
+	T data;
+	Node left;
+	Node right;
+	
+	public Node(T data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
